7346710fc5277565c89f0d65cda354d7d4fabbe7
Rafael dos Santos <rafaels1997@LIVE.COM>
2024-04-11 00:11:51 -0300

    Geração de resultado das rules com base em média ponderada

--- geração de jfuzzy/geração de jfuzzy.cpp
+++ geração de jfuzzy/geração de jfuzzy.cpp
@@ -21,20 +21,102 @@ int main()
 	if (arquivo.is_open()) {
 		#pragma omp parallel for
 		for (int a = 0; a < 3; a++) {
-			//string texto_porcentagem = "RULE " + to_string(rule) + " : IF porcentagem_assistida IS " + porcentagem_assistida[a];
 			for (int b = 0; b < 3; b++) {
-				//string texto_avaliacao = " AND avaliacao IS " + avaliacao[b];
 				for (int c = 0; c < 3; c++) {
-					//string texto_comentarios = " AND comentarios IS " + comentarios[c];
 					for (int d = 0; d < 3; d++) {
-						//string texto_tempo_de_video = " AND tempo_de_video IS " + tempo_de_video[d];
 						for (int e = 0; e < 3; e++) {
+
+							//Pesos para cada variavel
+							int peso_porcentagem_assistida = 2;
+							int peso_avaliacao = 3;
+							int peso_comentarios = 2;
+							int peso_tempo_de_video = 1;
+							int peso_visualizacoes = 2;
+							float a_convertido = 0;
+							float b_convertido = 0;
+							float c_convertido = 0;
+							float d_convertido = 0;
+							float e_convertido = 0;
+
+							//Conversao de variaveis
+							if (a == 0) {
+								a_convertido = 0;
+							}
+							else if (a == 1) {
+								a_convertido = 5;
+							}
+							else if (a == 2) {
+								a_convertido = 10;
+							}
+
+							if (b == 0) {
+								b_convertido = 0;
+							}
+							else if (b == 1) {
+								b_convertido = 5;
+							}
+							else if (b == 2) {
+								b_convertido = 10;
+							}
+
+							if (c == 0) {
+								c_convertido = 0;
+							}
+							else if (c == 1) {
+								c_convertido = 5;
+							}
+							else if (c == 2) {
+								c_convertido = 10;
+							}
+
+							if (d == 0) {
+								d_convertido = 0;
+							}
+							else if (d == 1) {
+								d_convertido = 5;
+							}
+							else if (d == 2) {
+								d_convertido = 10;
+							}
+
+							if (e == 0) {
+								e_convertido = 0;
+							}
+							else if (e == 1) {
+								e_convertido = 5;
+							}
+							else if (e == 2) {
+								e_convertido = 10;
+							}
+							
+							//Calculo da recomendacao
+							float recomendacao = ((peso_porcentagem_assistida * a_convertido) + (peso_avaliacao * b_convertido) + (peso_comentarios * c_convertido) + (peso_tempo_de_video * d_convertido) + (peso_visualizacoes * e_convertido)) / 10;
+							string recomendacao_final = "";
+
+							cout << to_string(recomendacao) + "\n";
+
+							if (recomendacao < 2) {
+								recomendacao_final = "pessima";
+							}
+							else if (recomendacao >= 2 && recomendacao < 4) {
+								recomendacao_final = "ruim";
+							}
+							else if (recomendacao >= 4 && recomendacao < 6) {
+								recomendacao_final = "media";
+							}
+							else if (recomendacao >= 6 && recomendacao < 8) {
+								recomendacao_final = "boa";
+							}
+							else if (recomendacao >= 8) {
+								recomendacao_final = "otima";
+							}
+
 							string texto = "RULE " + to_string(rule) + " : IF porcentagem_assistida IS " + porcentagem_assistida[a]
 								+ " AND avaliacao IS " + avaliacao[b]
 								+ " AND comentarios IS " + comentarios[c]
 								+ " AND tempo_de_video IS " + tempo_de_video[d]
 								+ " AND visualizacoes IS " + visualizacoes[e]
-								+ " THEN recomendacao IS \n";
+								+ " THEN recomendacao IS " + recomendacao_final + ";\n";
 							arquivo << texto;
 							rule++;
 						}
